17085a7b43a4e1a48497917d9c5722d2787f3070
Yassine Loukhnati <yloukhnati@whytespyder.com>
2023-08-24 10:24:00 -0400

    extracting components, adding proptypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
 import {useState} from 'react';
 import '../reset.css';
 import '../App.css';
+import NoTodos from './NoTodos';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 function App() {
-    const [todos,
-        setTodos] = useState([
+    const [todos, setTodos] = useState([
         {
             id: 1,
             title: 'Finish React Series',
             isComplete: false,
             isEditing: false
-        }, {
+        }, 
+        {
             id: 2,
             title: 'Go Grocery',
             isComplete: true,
             isEditing: false
-        }, {
+        }, 
+        {
             id: 3,
             title: 'Take over world',
             isComplete: false,
@@ -23,30 +27,20 @@ function App() {
         }
     ]);
 
-    const [todoInput,
-        setTodoInput] = useState('');
 
-    const [idForTodo,
-        setIdForTodo] = useState(4);
+    const [idForTodo, setIdForTodo] = useState(4);
 
-    function handleInput(event) {
-        setTodoInput(event.target.value);
-    }
 
-    function addTodo(event) {
-        event.preventDefault();
-        if (todoInput.trim().length === 0) {
-            return;
-        }
-        setTodos([
-            ...todos, {
-                id: idForTodo,
-                title: todoInput,
-                isComplete: false,
-                isEditing: false
+    function addTodo(todo) {
+
+        setTodos([...todos, {
+            id: idForTodo,
+            title: todo,
+            isComplete: false,
+            isEditing: false
             }
         ]);
-        setTodoInput('');
+
         setIdForTodo(previdForTodo => previdForTodo + 1);
     }
 
@@ -56,10 +50,10 @@ function App() {
 
     function completeTodo(todoId) {
         const updatedTodos = todos.map(todo => {
-            if (todo.id === todoId) {
-                todo.isComplete = !todo.isComplete
-            }
-            return todo;
+        if (todo.id === todoId) {
+            todo.isComplete = !todo.isComplete
+        }
+                return todo;
         });
 
         setTodos(updatedTodos);
@@ -70,7 +64,7 @@ function App() {
             if (todo.id === todoId) {
                 todo.isEditing = true
             }
-            return todo;
+                return todo;
         });
 
         setTodos(updatedTodos);
@@ -80,9 +74,9 @@ function App() {
         const updatedTodos = todos.map(todo => {
             if (todo.id === todoId) {
                 if (event.target.value.trim().length === 0) {
-                    todo.isEditing = false;
-                    return todo;
-                }
+                todo.isEditing = false;
+                return todo;
+            }
                 todo.title = event.target.value;
                 todo.isEditing = false;
             }
@@ -107,82 +101,9 @@ function App() {
         <div className="todo-app-container">
             <div className="todo-app">
                 <h2>Todo App</h2>
-                <form action="#" onSubmit={addTodo}>
-                    <input
-                        type="text"
-                        className="todo-input"
-                        value={todoInput}
-                        onChange={handleInput}
-                        placeholder="What do you need to do?"/>
-                </form>
-
-                <ul className="todo-list">
-                    {todos.map((todo, index) => (
-                        <li className="todo-item-container" key={todo.id}>
-                            <div className="todo-item">
-                                <input
-                                    type="checkbox"
-                                    onChange={() => completeTodo(todo.id)}
-                                    checked={todo.isComplete
-                                    ? true
-                                    : false}/> 
-                                    {!todo.isEditing ? (
-                                        <span
-                                            onDoubleClick={() => markAsEditing(todo.id)}
-                                            className={`todo-item-label ${todo.isComplete
-                                            ? 'line-through'
-                                            : ''}`}>{todo.title}
-                                        </span>
-
-                                    )
-                                    : (
-                                        <input type="text" className="todo-item-input" defaultValue={todo.title} onBlur={(event) => updateTodo(event, todo.id)} 
-                                        onKeyDown={event => {
-                                            if (event.key === "Enter") {
-                                                updateTodo(event, todo.id)
-                                            } else if (event.key === 'Escape') {
-                                                cancelEdit(event, todo.id)
-                                            }
-                                        }} autoFocus/>
-                                    )}
-                            </div>
-                            <button className="x-button" onClick={() => deleteTodo(todo.id)}>
-                                <svg
-                                    className="x-button-icon"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="check-all-container">
-                    <div>
-                        <div className="button">Check All</div>
-                    </div>
-
-                    <span>3 items remaining</span>
-                </div>
-
-                <div className="other-buttons-container">
-                    <div>
-                        <button className="button filter-button filter-button-active">
-                            All
-                        </button>
-                        <button className="button filter-button">Active</button>
-                        <button className="button filter-button">Completed</button>
-                    </div>
-                    <div>
-                        <button className="button">Clear completed</button>
-                    </div>
-                </div>
+                <TodoForm  addTodo={addTodo}/>
+                { todos.length > 0 ? <TodoList markAsEditing={markAsEditing} completeTodo={completeTodo} updateTodo={updateTodo}  cancelEdit={cancelEdit} deleteTodo={deleteTodo} todos={todos}/> : <NoTodos />
+                }
             </div>
         </div>
     );
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+TodoForm.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+}
+
+function TodoForm(props) {
+    const [todoInput, setTodoInput] = useState('');
+
+    function handleInput(event) {
+        setTodoInput(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (todoInput.trim().length === 0) {
+            return;
+        }
+
+        props.addTodo(todoInput);
+        setTodoInput('');
+    }
+    
+    return (
+        <form action="#" onSubmit={handleSubmit}>
+            <input type="text" className="todo-input" value={todoInput} onChange={handleInput}
+                placeholder="What do you need to do?" />
+        </form>
+    )
+}
+
+
+export default TodoForm
